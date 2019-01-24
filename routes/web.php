88336d90666d1b629375213af9d217285925f51a
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-01-24 12:29:53 +0100

    or movie, review and list make migrations, controllers and models, models need to include resource to be able to perform crudoperations.

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+// Route::get('/movies', 'MovieController@index');
+
+/*Dynamisk route */
+
+// Route::get('movies/{id}', function($id)
+// {
+//     return 'movie'.$id;
+// });
+
+/* routes */
+
+// Route::get($uri, $callback);
+// Route::post($uri, $callback);
+// Route::put($uri, $callback);
+// Route::patch($uri, $callback);
+// Route::delete($uri, $callback);
