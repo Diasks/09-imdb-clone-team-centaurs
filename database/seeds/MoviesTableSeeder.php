6a5986a895e44bb701a26fc7103d5d866609d56d
AdamBremler <adambremler@hotmail.com>
2019-03-03 01:09:50 +0100

    Add seeding from movie API, fix user list routes, install guzzle

--- database/seeds/MoviesTableSeeder.php
+++ database/seeds/MoviesTableSeeder.php
@@ -2,6 +2,8 @@
 
 use App\Models\Movie;
 use Illuminate\Database\Seeder;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
 
 class MoviesTableSeeder extends Seeder
 {
@@ -12,15 +14,35 @@ class MoviesTableSeeder extends Seeder
      */
     public function run()
     {
-        $movie = Movie::create([
-            'title' => "Fight Club",
-            'genres' => [
-                18,
-            ],
-            'runtime' => 139,
-            'release_date' => "1999-10-12",
-            'adult' => false,
-            'revenue' => 100853753,
-        ]);
+        $movieUrl = env('MOVIE_API_URL');
+        $movieKey = env('MOVIE_API_KEY');
+
+        $client = new Client(); // Guzzle client
+
+        for($id = 0; $id < 10; $id++) {
+            try {
+                $res = $client->request('GET', $movieUrl . $id . '?api_key=' . $movieKey);
+            }
+            
+            catch(Exception $e) {
+                continue;
+            }
+    
+            $resBody = json_decode($res->getBody(), true);
+    
+            $dataArr = [
+                'id' => $resBody['id'],
+                'title' => $resBody['title'],
+                'genres' => array_map(function($genre) {
+                    return $genre['id'];
+                }, $resBody['genres']),
+                'runtime' => $resBody['runtime'],
+                'release_date' => $resBody['release_date'],
+                'adult' => $resBody['adult'],
+                'revenue' => $resBody['revenue'],
+            ];
+    
+            $movie = Movie::create($dataArr);
+        }
     }
 }
--- routes/user.php
+++ routes/user.php
@@ -1,5 +1,5 @@
 <?php
 
 // List routes
-Route::get('/{user_id}/lists', 'ListController@index');
-Route::get('/{user_id}/lists/{id}', 'ListController@show');
+Route::get('/{user_id}/lists', 'MovieListController@index');
+Route::get('/{user_id}/lists/{id}', 'MovieListController@show');
