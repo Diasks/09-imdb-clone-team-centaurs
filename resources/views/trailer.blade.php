a399aa9ec7df675f5a6f24f346525c55e3f66111
Diana Skshipek <Duttydi@Dianas-MBP.lan>
2019-03-28 14:56:47 +0100

    add styling and button to go back to specific movie

--- resources/views/trailer.blade.php
+++ resources/views/trailer.blade.php
@@ -1,17 +1,28 @@
 
 
-<h1>Trailer for: {{$trailers->first()->title}}</h1>
+@extends('layouts.app')
+@section('content')
 
 
-@if (($trailers->first()->video) > 0)
-<video width="320" height="240" controls >
+
+
+
+<div class="jumbotron jumbotron-fluid bg-info">
+  <div class="container">
+    <h1 class="display-4">Trailer for: {{$trailers->first()->title}}</h1>
+    @if (($trailers->first()->video) > 0)
+    <video width="320" height="240" controls >
 <source src={{ $trailers->first()->video }} type="video/mp4">
 </video>
 @else 
 
-<h1>No trailer availabe!</h1>
+<h2>No trailer availabe for this movie!</h2>
 
 @endif
 
+  </div>
+  <a href="/movie/{{$trailers->first()->id}}"><button class="btn btn-dark">Go back</button></a>
 
+</div>
 
+@endsection
