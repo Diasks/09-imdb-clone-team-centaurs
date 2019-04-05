66167eaf3debe418900115fcf2a573ceee8151fe
Adam Bremler <adambremler@hotmail.com>
2019-04-05 19:22:10 +0200

    Add admin reviews UI

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -14,7 +14,7 @@ class AdminController extends Controller
      */
     public function reviews(Request $request)
     {
-        $reviews = Review::where('accepted', 0);
+        $reviews = Review::where('accepted', 0)->get();
 
         return view('admin-reviews', compact('reviews'));
     }
--- resources/views/admin-reviews.blade.php
+++ resources/views/admin-reviews.blade.php
@@ -2,6 +2,20 @@
 
 @section('content')
 
-.
+@forelse ($reviews as $review)
+    <div class="card mb-3">
+        <div class="card-header">
+            <h5>Movie: {{ $review->movie->title }}</h5>
+            <h5>User: {{ $review->author ? $review->author : $review->user->name }}</h5>
+        </div>
+        <div class="card-body">
+            <p class="card-text">{{ $review->content }}</p>
+            <button title="Accept" class="btn btn-success">✓</button>
+            <button title="Reject" class="btn btn-danger">✕</button>
+        </div>
+    </div>
+@empty
+    <div class="dropdown-item">No reviews to audit.</div>
+@endforelse
 
 @endsection
